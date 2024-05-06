76499636890efcab5f45481c70a56e3e25ccbfd0
jayantakumarnayak <jayantakumarnayak480@gmail.com>
2024-05-06 20:34:20 +0530

    frontend full

--- frontend/src/components/sidebar/LogoutButton.jsx
+++ frontend/src/components/sidebar/LogoutButton.jsx
@@ -1,19 +1,17 @@
-import React from "react";
 import useLogout from "../../hooks/useLogout";
 import { BiLogOut } from "react-icons/bi";
 
 const LogoutButton = () => {
-  const {loading,logout}=useLogout;
-  return (
-    <div className="mt-auto ">
-    {!loading ? (
-          <BiLogOut className="w-6 h-6 text-white cursor-pointer"
-          onClick={logout}/> 
-    ):(
-        <span className="loading loading-spinner"></span>
-    )}   
-    </div>
-  )
-}
+	const { loading, logout } = useLogout();
 
+	return (
+		<div className='mt-auto'>
+			{!loading ? (
+				<BiLogOut className='w-6 h-6 text-white cursor-pointer' onClick={logout} />
+			) : (
+				<span className='loading loading-spinner'></span>
+			)}
+		</div>
+	);
+};
 export default LogoutButton;
